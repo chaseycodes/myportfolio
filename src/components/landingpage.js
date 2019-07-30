dfe989a276f69cc2cba986fc6e4c4855a699a427
chaseahn <chaseahn4@gmail.com>
2019-07-30 14:55:07 -0400

    add css social links

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Component, Button } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Component } from 'react';
+import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button} from 'react-mdl';
 import Avatar from '../images/avatar.png';
 
 class LandingPage extends Component {
@@ -9,37 +9,89 @@ class LandingPage extends Component {
             <div style={styleSheet} >
                 <Grid className="landing-grid">
                     <Cell col={12}>
+
+                        {/* Avatar */}
                         <img 
                             src={Avatar}
                             alt="avatar"
                             className="avatar-image"
                         />
+
+                        {/* Description */}
                         <div className="banner-text">
                             <h1>
                                 Chase 
                             </h1>
                             <p>
-                                Lead Product Designer at Betfluent.
+                                Lead Product Designer at Betfluent
                             </p>
                             <p>
-                                View my past experiences here! 
+                                Click here to learn more about me  
                             </p>
                         </div>
+
+                        {/* Github */}
                         <div className="button-container">
-                            <button>
-                                hi
-                            </button>
+                            <a href="/aboutme" rel="noopener noreferrer" target="_black">
+                                <span></span>
+                                <span></span>
+                                <i class="fa fa-github" aria-hidden="true" />
+                                <text> github </text>
+                                <i class="fa fa-github" aria-hidden="true" />
+                            </a>
+                        </div>
+
+                        {/* Dribbble */}
+                        <div className="button-container">
+                            <a href="/aboutme" rel="noopener noreferrer" target="_black">
+                                <span></span>
+                                <span></span>
+                                <i class="fa fa-dribbble" aria-hidden="true" />
+                                <text> Dribbble </text>
+                                <i class="fa fa-dribbble" aria-hidden="true" />
+                            </a>
+                        </div>
+
+                        {/* LinkedIn */}
+                        <div className="button-container">
+                            <a href="/aboutme" rel="noopener noreferrer" target="_black">
+                                <span></span>
+                                <span></span>
+                                <i class="fa fa-linkedin-square" aria-hidden="true" />
+                                <text> Linkedin </text>
+                                <i class="fa fa-linkedin-square" aria-hidden="true" />
+                            </a>
                         </div>
                     </Cell>
+
+                    {/* Break */}
                     <Cell col={12}>
                         <div className="banner-text">
-                            <h1>Full Stack Web Developer</h1>
                             <hr/>
-                            <p>
-                                HTML/CASS/React/Python?ETC ALL THAT CRAP
-                            </p>
                         </div>
                     </Cell>
+
+                    <Cell col={12}>
+
+                        <div className="general-links">
+                            <a href="/">
+                                <div className="card">
+                                    <div>
+                                        <i class="fa fa-linkedin-square" aria-hidden="true" />
+                                    </div>
+                                    <text> Linkedin </text>
+                                </div>
+                            </a>
+                            <div className="card">
+                                hi
+                            </div>
+                            <div className="card">
+                                hi
+                            </div>
+                        </div>
+
+                    </Cell>
+
                 </Grid>
             </div>
         )
