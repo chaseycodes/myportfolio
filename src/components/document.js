ae1560b68aed8fabf775c6989eddad4c444c454d
chaseahn <chaseahn4@gmail.com>
2019-09-03 13:34:47 -0400

    complete link

--- src/components/document.js
+++ src/components/document.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 import resume from '../images/resume.png';
 
@@ -12,15 +13,18 @@ class Resume extends Component {
 
                     {/* Experience */}
                     <Cell col={12}>
+
+                        <Link to="/aboutme" className="link">
                         <div className="home-button">
                             <div className="circle-links">
                                 <ul><li>
-                                    <a href="/aboutme">
+                                    <a href="#">
                                         <i class="fa fa-reply" aria-hidden="true" />
                                     </a>
                                 </li></ul>
                             </div>
                         </div>
+                        </Link>
                         <div className="guide-container">
                             <img 
                                 src={resume}
